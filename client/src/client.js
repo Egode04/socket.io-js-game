5e2b15877c5471d52a7505c9dfe9b472441a30b6
Emil Gode <egode04@gmail.com>
2023-02-10 05:39:21 +0100

    first commit

---
+++ client/src/client.js
@@ -0,0 +1,5 @@
+const socket = io(`ws://localhost:8080`)
+
+socket.on('connect', () => {
+    console.log('connected')
+})
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+
+app.use(express.static(`${__dirname}/../client`))
+
+const server = http.createServer(app)
+const io = socket(server)
+
+io.on('connection', socket => {
+    console.log(`New client connected: ${socket.id}`)
+
+    // emit commands here
+    socket.on('message', msg => {
+        console.log(msg)
+    })
+})
+
+server.on('error', err => {
+    console.error(err)
+})
+
+server.listen(8080, () => {
+    console.log(`Server listening on port ${8080}`)
+})
